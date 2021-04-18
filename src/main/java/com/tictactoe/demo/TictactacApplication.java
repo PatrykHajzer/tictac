bb3985a0fb6fe0778e0bb28235404268fda28b7b
Patryk <phajzer@oakfusion.pl>
2021-04-18 19:47:37 +0200

    init

---
+++ src/main/java/com/tictactoe/demo/TictactacApplication.java
@@ -0,0 +1,13 @@
+package com.tictactoe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactacApplication.class, args);
+    }
+
+}
